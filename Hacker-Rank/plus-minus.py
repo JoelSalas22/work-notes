438c2401d3fb39d56e8c6a70130a7c9f905b4ca7
JoelSalas <salas.22@buckeyemail.osu.edu>
2024-01-06 17:45:06 -0500

    Update plus-minus.py
    Added Example

--- Hacker-Rank/plus-minus.py
+++ Hacker-Rank/plus-minus.py
@@ -1,3 +1,26 @@
+    """
+     Complete the 'plusMinus' function below.
+     
+     The function accepts INTEGER_ARRAY arr as parameter.
+     Given an array of integers, calculate the ratios of its 
+     elements that are positive, negative, and zero. 
+     Print the decimal value of each fraction on a new line with
+     places after the decimal.
+
+    Note: This challenge introduces precision problems. 
+    The test cases are scaled to six decimal places, though 
+    answers with absolute error of up to
+    are acceptable.
+
+    
+    ########### EXAMPEL ###########
+    arr = [1, 1, 0, -1, -1]
+    arrLength = 5
+    postive integers = 2 / arrLength -> 0.400000
+    negative integers = 2 / arrLength -> 0.400000
+    zeros = 1 / arrLength -> 0.200000
+    """
+
 def plusMinus(arr):
     # Write your code here
     length_of_array = len(arr)
